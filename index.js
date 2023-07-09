39ce3a8c3f229e02b01d71d465856585ed0ebcab
Omr-pseudo <uali2407@gmail.com>
2023-07-09 23:17:18 +0500

    initial commit

--- index.js
+++ index.js
@@ -2,25 +2,105 @@ var express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const routes = require('./src/routes/crmRoutes');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
-//const routes = require('./src/routes/crmRoutes');
 
-app.use(function(req,res,next){
+mongoose.connect('mongodb://127.0.0.1/crmData', {
+    useNewUrlParser: true
+});
 
-   console.log("Time:", Date.now());
-   next();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+const blogSchema = require('./src/model/crmModel');
+const blogModel = mongoose.model("Blog", blogSchema);
+
+
+app.use(express.static('public'));
+
+
+//POST new blog
+app.post('/newBlog', (req,res) => {
+
+    let blog = new blogModel(req.body);
+
+    blog.save()
+    .then(blogModel =>{
+
+        res.json(blog);
+    })
+    .catch(err => res.send(err));
 })
 
-app.route('/').get(function(req,res,next){
-    console.log("Method", req.method);
-    next();
-}, function(req,res,next){
-    console.log("Original url", req.originalUrl)
-    next();
-}, function(req,res,next){
-    res.send("This is the get request being served");
-})
+//GET all blogs
 
+const getAllBlogs = (req, res) => {
+
+    blogModel.find({}).then( blogs => {
+        res.json(blogs);
+    })
+    .catch(err => {
+        res.send(err);
+    })
+}
+
+app.get("/getBlogs", getAllBlogs);
+
+
+//GET blog by param 
+const getBlogByID = (req,res) => 
+{
+    blogModel.findById(req.params.blogId)
+    .then( blog => {
+        res.json(blog);
+    })
+    .catch(err => {
+        res.send(err);
+    })
+}
+
+app.get('/blog/:blogId', getBlogByID);
+
+
+//PUT update blog
+const updateBlog = (req,res)=>{
+
+    blogModel.findOneAndUpdate({_id: req.params.blogId}, req.body, {new: true})
+
+    .then(updatedBlog => {
+        res.json(updatedBlog);
+    })
+
+    .catch(err => {
+        res.send(err);
+    })
+}
+
+app.put('/blog/:blogId', updateBlog);
+
+
+
+//Delete blog by param
+
+const deleteBlog = (req,res) => {
+    blogModel.findOneAndDelete({_id: req.params.blogId})
+
+    .then( () => {
+        res.json({message: "Success!!"});
+    })
+
+    .catch( err => {
+        res.send(err);
+    })
+}
+
+app.delete('/blog/:blogId', deleteBlog);
+
+
+
+// Server
 app.listen(PORT, () =>{
     console.log(`Your server is running on port: ${PORT}`)
 });
